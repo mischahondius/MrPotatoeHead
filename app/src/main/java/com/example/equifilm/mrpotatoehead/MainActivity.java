7495557ce67961fba7588562119842338bdbae30
Mischa Hondius <mhondius@gmail.com>
2015-02-12 16:54:21 +0100

    Final

--- app/src/main/java/com/example/equifilm/mrpotatoehead/MainActivity.java
+++ app/src/main/java/com/example/equifilm/mrpotatoehead/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.equifilm.mrpotatoehead;
 
+import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +43,149 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void toggleBodyPart(String id){
 
-        String bodyPart = id;
+    public void checkBoxClicked(View view) {
+        //declare img imageview
+        ImageView img;
 
-        //get plaatje by id
-        ImageView plaatje = (ImageView)findViewById(R.id.bodyPart);
+        // check if checkbox is checked
+        boolean checked = ((CheckBox) view).isChecked();
 
-        //toggle visibility
-        plaatje.setVisibility(View.VISIBLE);
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+
+            case R.id.arms_Checkbox:
+                //Get arms view
+                img = (ImageView) findViewById(R.id.arms);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+            case R.id.ears_Checkbox:
+                //Get ears view
+                img = (ImageView) findViewById(R.id.ears);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+            case R.id.eyebrows_Checkbox:
+                //Get arms view
+                img = (ImageView) findViewById(R.id.eyebrows);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+
+            case R.id.eyes_Checkbox:
+                //Get eyes view
+                img = (ImageView) findViewById(R.id.eyes);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+            case R.id.glasses_Checkbox:
+                //Get glasses view
+                img = (ImageView) findViewById(R.id.glasses);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+            case R.id.hat_Checkbox:
+                //Get arms view
+                img = (ImageView) findViewById(R.id.hat);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+            case R.id.mouth_Checkbox:
+                //Get mouth view
+                img = (ImageView) findViewById(R.id.mouth);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+            case R.id.mustache_Checkbox:
+                //Get mustache view
+                img = (ImageView) findViewById(R.id.mustache);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+            case R.id.nose_Checkbox:
+                //Get arms nose
+                img = (ImageView) findViewById(R.id.nose);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+            case R.id.shoes_Checkbox:
+                //Get shoes view
+                img = (ImageView) findViewById(R.id.shoes);
+
+                if (checked) {
+                    //Set visibility to visible
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    //Set visibility to visible
+                    img.setVisibility(View.INVISIBLE);
+                }
+                break;
+
+        }
     }
-
 
 }
